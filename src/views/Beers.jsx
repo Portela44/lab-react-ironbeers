793efd8674f01eef28a0cd51a7385a98adef66a9
Guillem <guillem.mart7@gmail.com>
2022-08-26 21:07:16 +0200

    iteration 3 done

--- src/views/Beers.jsx
+++ src/views/Beers.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from "axios"
+import {Link} from "react-router-dom"
 
 export default function Beers() {
-  return (
-    <div>Beers</div>
-  )
+    const [beers, setBeers] = useState(null);
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const response = await axios.get("https://ih-beers-api2.herokuapp.com/beers");
+                console.log(response.data);
+                setBeers(response.data)
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        getData();
+    },[]);
+
+    return (
+        <div className="beerList">
+            {beers && beers.map(beer => {
+                return (
+                    <div className="beer" key={beer._id}>
+                        <div className="beerImg">
+                            <Link to={`/:id`}>
+                                <img src={beer.image_url} alt="{beer.name}"></img>
+                            </Link>
+                        </div>
+                        <div className="beerInfo">
+                            <h3>{beer.name}</h3>
+                            <p>{beer.tagline}</p>
+                            <h5>Created by: <span>{beer.name}</span></h5>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    )
 }
